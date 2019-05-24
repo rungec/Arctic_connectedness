7b12c5c1a62a36420b769240c8d608fda9d14501
rungec <cru016@uit.no>
2019-05-24 16:04:28 +0200

    added processing for Canada

--- PPGIS_data_formatting.r
+++ PPGIS_data_formatting.r
@@ -69,7 +69,7 @@ idcodereplace <- lapply(dirlist, function(shpdir) {
     currshp[, "Id"] <- idcode[1]
     
     #save shp
-    st_write(currshp, paste0("PPGIS_CONNECT/Processed/CONNECT/Russia/", basename(currfile)))
+    st_write(currshp, paste0("PPGIS_CONNECT/Processed/CONNECT/Russia/intermediate/", basename(currfile)))
   }
   return(codeDF)
 } ) 
@@ -78,7 +78,7 @@ write.csv(idcodereplace, paste0("PPGIS_CONNECT/Processed/CONNECT/Russia/checkidc
 
 # 2. Merge shps #### 
 #they have different numbers of columns so we do a bit of pre-processing
-procfiles <- list.files("PPGIS_CONNECT/Processed/CONNECT/Russia/", "*.shp$", full.names=TRUE)
+procfiles <- list.files("PPGIS_CONNECT/Processed/CONNECT/Russia/intermediate/", "*.shp$", full.names=TRUE)
 # make a list of all the column headers in the .shps
 coldf <- data.frame()
 currnames <- c()
@@ -90,7 +90,7 @@ for(i in procfiles) {
 }
 uniquecols <- unique(currnames)
 
-procfiles <- list.files("PPGIS_CONNECT/Processed/CONNECT/Russia/", "*.shp$", full.names=TRUE)
+procfiles <- list.files("PPGIS_CONNECT/Processed/CONNECT/Russia/intermediate/", "*.shp$", full.names=TRUE)
 # make all the shps have the same column headers
 tempfileList <- lapply(procfiles, function (x) {
   currshp <-  read_sf(x)
@@ -108,13 +108,13 @@ tempfileList <- lapply(procfiles, function (x) {
 #For polygons ####
 polids <- which(grepl("*pol.shp$", procfiles))
 russiashp_pol <- do.call(rbind, tempfileList[polids])
-write_sf(russiashp_pol, "PPGIS_CONNECT/Processed/CONNECT/Russia/combined/Russia_allpolygons_stereopolarWGS84.shp")
+write_sf(russiashp_pol, "PPGIS_CONNECT/Processed/CONNECT/Russia/output/Russia_allpolygons_stereopolarWGS84.shp")
 
 #finally merge the .shps
 #For lines ####
 linids <- which(grepl("*lin.shp$", procfiles))
 russiashp_lin <- do.call(rbind, tempfileList[linids])
-write_sf(russiashp_lin, "PPGIS_CONNECT/Processed/CONNECT/Russia/combined/Russia_alllines_stereopolarWGS84.shp")
+write_sf(russiashp_lin, "PPGIS_CONNECT/Processed/CONNECT/Russia/output/Russia_alllines_stereopolarWGS84.shp")
 
 #3. reproject to lambert azimuthal ####
 #this was done in ArcGIS (checked preserve shape; because I don't trust R to do it correctly)
@@ -124,47 +124,85 @@ write_sf(russiashp_lin, "PPGIS_CONNECT/Processed/CONNECT/Russia/combined/Russia_
 ###############
 #These are in GCS_North_America_1983 which is EPSG:4269
 
+#1. Merge was done in ArcGIS, but here is some code to do it in R ####
 #filelist <- list.files("PPGIS_CONNECT/Raw/TUNDRA_Shapefiles_Canada", pattern="append", full.names = TRUE) 
 #filelist <- grep(".shp$", filelist, value=TRUE)
 #shpList <- lapply(filelist, function(currfile) read_sf(currfile))
 #canadaShp <- do.call(rbind, shpList)
 #st_write(canadaShp, paste0("PPGIS_CONNECT/Processed/CONNECT/Canada/"))
 
-#Merge shps was done in ArcGIS because it is quicker
-#reproject to lambert azimuthal - this was done in ArcGIS (checked preserve shape; because I don't trust R to do it correctly)
+#2. reproject to lambert azimuthal ####
+#this was done in ArcGIS (checked preserve shape; because I don't trust R to do it correctly)
 
-#reformat columns
-canadaShp <- read_sf("PPGIS_CONNECT/Processed/Canada/Canada_allpolygons_laea.shp")
+#3. reformat columns ####
+#load file
+canadaShp <- read_sf(dsn="PPGIS_CONNECT/Processed/CONNECT/Canada.gdb", "Canada_allpolygons_laea")
+
+#rename columns
+names(canadaShp)[which(names(canadaShp) == "Id")] <- "AA_Id"
+names(canadaShp)[which(names(canadaShp) == "UniqueID")] <- "AA_Navn"
+
+#split the polygon id from column UniqueID2 (format = 6 digit interviewee id 2 digit polygon id). Add 1 because they start at zero 
+canadaShp$AA_Number <- (as.numeric(do.call(rbind, lapply(canadaShp$UniqueID2, function (x) str_split(x, "_")[[1]][3]))) +1)
+
+#Split cabin & camp 
+canadaShp <- canadaShp %>%
+              mutate(L_Cabin = case_when(Camp_Cabin %in% c("Cabin", "Camp and Cabin", "Cabin/Camp") ~ 1),
+                     L_Camp = case_when(Camp_Cabin %in% c("Camp", "Camp and Cabin", "Cabin/Camp") ~ 1))
+
+#split Harvest ID 
+#unique(canadaShp$HarvestID)
+#write.csv(unique(canadaShp$HarvestID), "PPGIS_CONNECT/Raw/Reclass_of_harvestIDs_canada.csv", #row.names=FALSE)
+#I went through these manually and renamed them according to the Norwegian letter convention eg. A_Caribou
+harvestcode <- read.csv("PPGIS_CONNECT/Raw/Reclass_of_harvestIDs_canada.csv")
+canadaShp <- merge(canadaShp, harvestcode, by.x="HarvestID",by.y="harvestID", all.x=TRUE)
+
+canadaShp2 <- canadaShp %>% 
+              mutate(colval=1) %>%
+              spread(New_col_name, value = colval) 
+
+#save 
+canadaShp2 <- canadaShp2[, -which(names(canadaShp2) %in% c("Shape_Length","Shape_Area", "V1"))] #drop unnecesary columns
+write_sf(canadaShp2, "PPGIS_CONNECT/Processed/CONNECT/Canada/Canada_allpolygons_laea_reformated.shp")
+
+
+#pull I_ who they go with from notes
+#N_Nights from Length column
+#N_Length from Frequency and from Notes
+#N_Season from Frequency and Notes
+#G what they do from Notes
+#H how they travel from Notes
+
 
 ###############
 #Alaska
 ###############
 #reproject to lambert azimuthal - this was done in ArcGIS (checked preserve shape; because I don't trust R to do it correctly)
 
-# Read the feature class
+# Read the shapefile
 alaskaShp <- read_sf(dsn="PPGIS_CONNECT/Processed/CONNECT/Alaska.gdb",layer="Alaska_allpolygons_laea")
 
-#rename column headers
+#rename column headers based on excel spreadsheet
 names(alaskaShp)[1:49] <- colheaders$Alaska_rename[1:49]
 
+#extract the idcode to match
+alaskaShp$AA_Id <- unlist(lapply(alaskaShp$AA_Navn, function(x) return(interviewDF$ID[which(interviewDF$Interviewee==x)])))
 
 #split the polygon id from column UniqueID2 (format = 6 digit interviewee id 2 digit polygon id). Add 1 because they start at zero
 alaskaShp$AA_Number <- (as.numeric(do.call(rbind, lapply(alaskaShp$UniqueID2, function (x) str_split(x, "_")[[1]][3]))) +1)
 
-#split cabin and camp
+#save
+alaskaShp <- alaskaShp[, -which(names(alaskaShp) %in% c("Shape_Length","Shape_Area"))] #drop unnecesary columns
+write_sf(alaskaShp, "PPGIS_CONNECT/Processed/CONNECT/Alaska/Alaska_allpolygons_laea_reformated.shp")
 
+
+# cabin from notes
 #what to do with duplicated columns?
-
 #pull I_ who they go with from notes
-
 #N_Nights from Length column
-
 #N_Length from Frequency and from Notes
-
 #N_Season from Frequency and Notes
-
 #G what they do from Notes
-
 #H how they travel from Notes
 
 
